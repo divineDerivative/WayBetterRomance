844b3a34626cb47df01c5241dee43c0627d62d54
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2022-11-03 19:12:09 -0500

    Patch for Word of Love

---
+++ Source/Harmony/CompAbilityEffect_WordOfLove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using HarmonyLib;
+using System.Reflection.Emit;
+
+namespace BetterRomance
+{
+    [HarmonyPatch(typeof(CompAbilityEffect_WordOfLove), "ValidateTarget")]
+    public static class CompAbilityEffect_WordOfLove_ValidateTarget
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            bool firstFound = false;
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if (instruction.Is(OpCodes.Ldc_R4, 16f) && !firstFound)
+                {
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return CodeInstruction.Call(typeof(SettingsUtilities), nameof(SettingsUtilities.MinAgeForSex));
+                }
+                else if (instruction.Is(OpCodes.Ldc_R4, 16f))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldloc_1);
+                    yield return CodeInstruction.Call(typeof(SettingsUtilities), nameof(SettingsUtilities.MinAgeForSex));
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}
