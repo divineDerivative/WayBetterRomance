ed82277677167d9ac11fc92cce18c379f08ef7ed
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2023-01-12 19:55:17 -0600

    Compatibility for VSIE developing traits

---
+++ Source/Harmony/TraitSet_GainTrait.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace BetterRomance.HarmonyPatches
+{
+    [HarmonyPatch(typeof(TraitSet), nameof(TraitSet.GainTrait))]
+    public static class TraitSet_GainTrait
+    {
+        public static bool Prefix(Trait trait, ref Trait __state, Pawn ___pawn, bool suppressConflicts = false)
+        {
+            //Only care about orientation traits
+            if (RomanceUtilities.OrientationTraits.Contains(trait.def))
+            {
+                //Check if pawn already has one
+                foreach (var traitDef in RomanceUtilities.OrientationTraits)
+                {
+                    if (___pawn.story.traits.HasTrait(traitDef))
+                    {
+                        //Remember it for later
+                        __state = ___pawn.story.traits.GetTrait(traitDef);
+                        return true;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static void Postfix(Trait trait, ref Trait __state, Pawn ___pawn, bool suppressConflicts = false)
+        {
+            if (__state != null)
+            {
+                //Check if a second orientation trait got added
+                int traitCount = 0;
+                foreach (var traitDef in RomanceUtilities.OrientationTraits)
+                {
+                    if (___pawn.story.traits.HasTrait(traitDef))
+                    {
+                        traitCount++;
+                    }
+                }
+                //Remove the old trait
+                if (traitCount > 1)
+                {
+                    ___pawn.story.traits.RemoveTrait(__state, suppressConflicts);
+                }
+            }
+        }
+    }
+}
