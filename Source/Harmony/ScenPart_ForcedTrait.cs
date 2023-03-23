9636507a3522aaee3d3304762cf269a7f86f0f33
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2023-03-23 10:45:00 -0500

    Update ScenPart_ForcedTrait.cs

--- Source/Harmony/ScenPart_ForcedTrait.cs
+++ Source/Harmony/ScenPart_ForcedTrait.cs
@@ -1,25 +1,21 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using RimWorld;
 
 namespace BetterRomance.HarmonyPatches
 {
-    [HarmonyPatch]
-    public static class ScenPart_ForcedTrait_Patches
+    [HarmonyPatch(typeof(ScenPart_ForcedTrait), "PawnHasTraitForcedByBackstory")]
+    public static class ScenPart_ForcedTrait_PawnHasTraitForcedByBackstory
     {
         // ScenPart_ForcedTrait picks a trait to remove when it adds it's chosen trait.
         // We can patch here and make sure that Orientation traits are not removed by
         // this ScenPart (forcing it to pick a different trait instead)
-        [HarmonyPatch(typeof(ScenPart_ForcedTrait), "PawnHasTraitForcedByBackstory")]
-        [HarmonyPostfix]
-        public static void ScenPart_ForcedTrait_PawnHasTraitForcedByBackstory_Postfix(TraitDef trait, ref bool __result)
+
+        public static void Postfix(TraitDef trait, ref bool __result)
         {
-            if(!__result) // If it's already true, then it's a Backstory trait and can't be removed anyway
+            // If it's already true, then it's a Backstory trait and can't be removed anyway
+            if (!__result)
             {
-                var ret = RomanceUtilities.OrientationTraits.Contains(trait);
-                if (ret)
-                {
-                    __result = true;
-                }
+                __result = RomanceUtilities.OrientationTraits.Contains(trait);
             }
         }
     }
