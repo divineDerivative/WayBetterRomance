8d4dd97c3c627d26c5c59e7f49fef8c3829be2e3
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2023-03-23 11:04:54 -0500

    Update TweaksGalore_Patches.cs

--- Source/Harmony/ModCompatibility/TweaksGalore_Patches.cs
+++ Source/Harmony/ModCompatibility/TweaksGalore_Patches.cs
@@ -1,5 +1,6 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using RimWorld;
+using System.Reflection;
 
 namespace BetterRomance.HarmonyPatches
 {
@@ -7,19 +8,15 @@ namespace BetterRomance.HarmonyPatches
     {
         public static void PatchTweaksGalore(this Harmony harmony)
         {
-            var tgIsSexualityTrait = typeof(TweaksGalore.Patch_PawnGenerator_GenerateTraits).GetMethod(nameof(TweaksGalore.Patch_PawnGenerator_GenerateTraits.IsSexualityTrait));
-            var prefixIsSexualityTrait = typeof(TG_PawnGenerator_GenerateTraits_IsSexualityTrait).GetMethod(nameof(TG_PawnGenerator_GenerateTraits_IsSexualityTrait.Prefix));
-            harmony.Patch(tgIsSexualityTrait, prefix: new HarmonyMethod(prefixIsSexualityTrait));
+            MethodInfo tgIsSexualityTrait = AccessTools.Method("TweaksGalore.Patch_PawnGenerator_GenerateTraits:IsSexualityTrait");
+            harmony.Patch(tgIsSexualityTrait, prefix: new HarmonyMethod(typeof(TweaksGalore_Patches), nameof(TweaksGalore_Patches.IsSexualityTraitPrefix)));
             LogUtil.Message("TweaksGalore patches applied.");
         }
-    }
-    
-    public static class TG_PawnGenerator_GenerateTraits_IsSexualityTrait
-    {
+
         // Override TweaksGalore's IsSexualityTrait method that hard-codes the base-game traits only (Gay, Bisexual, Asexual)
-        public static bool Prefix(Trait trait, ref bool __result)
+        public static bool IsSexualityTraitPrefix(Trait trait, ref bool __result)
         {
-            var ret = RomanceUtilities.OrientationTraits.Contains(trait.def);
+            bool ret = RomanceUtilities.OrientationTraits.Contains(trait.def);
             __result = ret;
             return false;
         }
