6d58b01a4f7fe375484a42a200f82db227849753
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2023-03-24 17:27:17 -0500

    Basic files that were not previously included in repository

---
+++ Source/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:BetterRomance.HarmonyPatches.InteractionWorker_RomanceAttempt_TryAddCheaterThought.Prefix(Verse.Pawn,Verse.Pawn)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:BetterRomance.HarmonyPatches.LovePartnerRelationUtility_TryToShareChildrenForGeneratedLovePartner.Prefix(Verse.Pawn,Verse.Pawn,Verse.PawnGenerationRequest,System.Single)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:BetterRomance.HarmonyPatches.InteractionWorker_Breakup_Interacted.Prefix(Verse.Pawn,Verse.Pawn,System.Collections.Generic.List{Verse.RulePackDef},System.String@,System.String@,Verse.LetterDef@,Verse.LookTargets@,RimWorld.InteractionWorker_Breakup)~System.Boolean")]
