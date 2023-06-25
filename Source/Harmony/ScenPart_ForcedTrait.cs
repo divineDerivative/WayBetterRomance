cd662a26daf3585b40375f24b032546220e427a6
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2023-06-24 19:50:46 -0500

    Recognition of additional orientation traits

---
+++ Source/Harmony/ScenPart_ForcedTrait.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using HarmonyLib;
+
+namespace BetterRomance.HarmonyPatches
+{
+    [HarmonyPatch(typeof(ScenPart_ForcedTrait), "PawnHasTraitForcedByBackstory")]
+    public static class ScenPart_ForcedTrait_PawnHasTraitForcedByBackstory
+    {
+        public static void Postfix(TraitDef trait, ref bool __result)
+        {
+            if (!__result) // If it's already true, then it's a Backstory trait and can't be removed anyway
+            {
+                __result = RomanceUtilities.OrientationTraits.Contains(trait);
+            }
+        }
+    }
+}
