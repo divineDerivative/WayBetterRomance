5d0f5b570321fce6b037e625fb8b3d2f6bf7531d
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2023-07-22 14:17:18 -0500

    Patch for ageless gene to use age settings

--- Source/Harmony/Pawn_GeneTracker.cs
+++ Source/Harmony/Pawn_GeneTracker.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using RimWorld;
 using Verse;
 using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace BetterRomance.HarmonyPatches
 {
@@ -19,6 +17,43 @@ namespace BetterRomance.HarmonyPatches
                 SettingsUtilities.CachedNonSenescentPawns.Remove(__instance.pawn);
                 SettingsUtilities.CachedSenescentPawns.Remove(__instance.pawn);
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_GeneTracker), nameof(Pawn_GeneTracker.BiologicalAgeTickFactor), MethodType.Getter)]
+    public static class Pawn_GeneTracker_BiologicalAgeTickFactor
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            FieldInfo biologicalAgeTickFactorFromAgeCurve = AccessTools.Field(typeof(GeneDef), nameof(GeneDef.biologicalAgeTickFactorFromAgeCurve));
+
+            foreach (CodeInstruction code in instructions)
+            {
+                if (code.LoadsField(biologicalAgeTickFactorFromAgeCurve))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return CodeInstruction.LoadField(typeof(Pawn_GeneTracker), nameof(Pawn_GeneTracker.pawn));
+                    yield return CodeInstruction.Call(typeof(Pawn_GeneTracker_BiologicalAgeTickFactor), nameof(CurveHelper));
+                }
+                else
+                {
+                    yield return code;
+                }
+            }
+        }
+
+        private static SimpleCurve CurveHelper(GeneDef geneDef, Pawn pawn)
+        {
+            if (geneDef.defName == "Ageless")
+            {
+                return new SimpleCurve
+                {
+                    new CurvePoint(pawn.ageTracker.AdultMinAge, 1f),
+                    new CurvePoint(SettingsUtilities.AdultAgeForLearning(pawn) + 0.5f, 0f)
+                };
+            }
+
+            return geneDef.biologicalAgeTickFactorFromAgeCurve;
         }
     }
 }
