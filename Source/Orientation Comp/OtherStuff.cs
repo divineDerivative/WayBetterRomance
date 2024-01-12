6d147a948004405d229d3e088ad9b2e4ab4b6349
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-01-11 21:24:01 -0600

    Basic comp

---
+++ Source/Orientation Comp/OtherStuff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace BetterRomance
+{
+    //Make a new trait for orientation
+    //Make a method on the comp to determine the correct label
+    //Then make a patch for TraitDegreeData.GetLabelFor and GetLabelForCap to pass that label
+    //Make a method on the comp to determine the correct description
+    //Then make a patch for Trait.TipString to pass the correct description
+    //Postfix that finds the description from the xml and replaces it
+}
