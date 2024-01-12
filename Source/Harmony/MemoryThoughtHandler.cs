72ca2c7447acce158526f05edd918365dde3404d
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-01-11 19:56:20 -0600

    Same adjustments for the modded lovin' thoughts

--- Source/Harmony/MemoryThoughtHandler.cs
+++ Source/Harmony/MemoryThoughtHandler.cs
@@ -35,8 +35,9 @@ namespace BetterRomance.HarmonyPatches
             {
                 if (___pawn.IsAsexual())
                 {
-                    int moodOffset = (int)GenMath.LerpDouble(0f, 1f, -10f, 10f, ___pawn.AsexualRating());
-                    float opinionOffset = GenMath.LerpDouble(0f, 1f, -8f, 8f, ___pawn.AsexualRating());
+                    //Making floor lower so 0.2f, which is the lowest rating that should get this thought, gets the correct modifiers, -10 and -8
+                    int moodOffset = (int)GenMath.LerpDouble(0f, 1f, -15f, 10f, ___pawn.AsexualRating());
+                    float opinionOffset = GenMath.LerpDouble(0f, 1f, -12f, 8f, ___pawn.AsexualRating());
                     Thought_MemorySocial replacementThought = ThoughtMaker.MakeThought((moodOffset < 0) ? RomanceDefOf.PassionateLovinAsexualNegative : RomanceDefOf.PassionateLovinAsexualPositive) as Thought_MemorySocial;
 
                     if (RomanceUtilities.HasLoveEnhancer(___pawn) || RomanceUtilities.HasLoveEnhancer(otherPawn))
