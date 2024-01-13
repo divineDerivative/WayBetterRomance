f3a0edf77aa8718944962db73b37905da6d030bf
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-01-13 13:40:33 -0600

    Converting and assigning comp

--- Source/Orientation Comp/OtherStuff.cs
+++ Source/Orientation Comp/OtherStuff.cs
@@ -12,4 +12,5 @@ namespace BetterRomance
     //Make a method on the comp to determine the correct description
     //Then put that in the patch to pass the correct description
     //Postfix that finds the description from the xml and replaces it
+    //Maybe detect an Individuality comp and convert that?
 }
