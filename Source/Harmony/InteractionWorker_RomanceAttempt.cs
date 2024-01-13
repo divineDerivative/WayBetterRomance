daba5069dac9d7fb953ddd2e39aa4224ee0a2ca6
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-01-13 16:00:21 -0600

    Replacing old orientation checks with comp checks

--- Source/Harmony/InteractionWorker_RomanceAttempt.cs
+++ Source/Harmony/InteractionWorker_RomanceAttempt.cs
@@ -266,6 +266,7 @@ namespace BetterRomance.HarmonyPatches
                     //num = RomanceUtilities.SexualityFactor(romanceTarget, romancer);
                     yield return new CodeInstruction(OpCodes.Ldarg_1).WithLabels(newLabel);
                     yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Ldc_I4_1);
                     yield return CodeInstruction.Call(typeof(RomanceUtilities), nameof(RomanceUtilities.SexualityFactor));
                     yield return new CodeInstruction(OpCodes.Stloc, num);
                     //if (num != 1f)
