c0c0f1470dbbbaf13944e80d7dbf8d83232758d4
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-01-15 11:39:00 -0600

    Removing unneeded things + small changes

--- Source/Sex Repulsion/Trait.cs
+++ Source/Sex Repulsion/Trait.cs
@@ -5,14 +5,14 @@ using Verse;
 
 namespace BetterRomance.HarmonyPatches
 {
-    //Adds 'Click for more info' to the end of the description of asexual traits
+    //Adds 'Click for more info' to the end of the description of orientation traits
     [HarmonyPatch(typeof(Trait), nameof(Trait.TipString))]
     public static class Trait_TipString
     {
         [HarmonyPostfix]
         public static void AsexualPostfix(Pawn pawn, ref string __result, Trait __instance)
         {
-            if (OrientationUtility.asexualTraits.Contains(__instance.def) && Current.ProgramState == ProgramState.Playing)
+            if (__instance.def == RomanceDefOf.DynamicOrientation && Current.ProgramState == ProgramState.Playing)
             {
                 StringBuilder str = new(__result);
                 str.AppendLine();
