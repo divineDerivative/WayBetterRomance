587a620bb3fdccd7b9ba8f7fdfc9bda05dc71341
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-07-07 11:42:17 -0500

    Protect against fatal errors
    while patching other mods

--- Source/OnStartup.cs
+++ Source/OnStartup.cs
@@ -29,10 +29,17 @@ namespace BetterRomance
             }
             if (ModsConfig.IsActive("telardo.romanceontherim"))
             {
-                Settings.RotRActive = true;
-                HelperClasses.RotRFillRomanceBar = AccessTools.DeclaredMethod(Type.GetType("RomanceOnTheRim.RomanceUtility,RomanceOnTheRim"), "TryAffectRomanceNeedLevelForPawn");
-                HelperClasses.RotRPreceptExplanation = AccessTools.Method(Type.GetType("RomanceOnTheRim.HarmonyPatch_SocialCardUtility_RomanceExplanation,RomanceOnTheRim"), "PreceptExplanation");
-                harmony.PatchRotR();
+                try
+                {
+                    Settings.RotRActive = true;
+                    HelperClasses.RotRFillRomanceBar = AccessTools.DeclaredMethod(Type.GetType("RomanceOnTheRim.RomanceUtility,RomanceOnTheRim"), "TryAffectRomanceNeedLevelForPawn");
+                    HelperClasses.RotRPreceptExplanation = AccessTools.Method(Type.GetType("RomanceOnTheRim.HarmonyPatch_SocialCardUtility_RomanceExplanation,RomanceOnTheRim"), "PreceptExplanation");
+                    harmony.PatchRotR();
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching Romance on the Rim: {ex}");
+                }
             }
             if (ModsConfig.IsActive("dylan.csl"))
             {
@@ -40,77 +47,147 @@ namespace BetterRomance
             }
             if (ModsConfig.IsActive("Killathon.AndroidTiersReforged"))
             {
-                Settings.ATRActive = true;
-                HelperClasses.IsConsideredMechanicalAndroid = AccessTools.DeclaredMethod(Type.GetType("ATReforged.Utils,Android Tiers Reforged"), "IsConsideredMechanicalAndroid", [typeof(Pawn)]);
-                HelperClasses.IsConsideredMechanicalDrone = AccessTools.DeclaredMethod(Type.GetType("ATReforged.Utils,Android Tiers Reforged"), "IsConsideredMechanicalDrone", [typeof(Pawn)]);
-                HelperClasses.IsConsideredMechanical = AccessTools.DeclaredMethod(Type.GetType("ATReforged.Utils,Android Tiers Reforged"), "IsConsideredMechanical", [typeof(ThingDef)]);
+                try
+                {
+                    Settings.ATRActive = true;
+                    HelperClasses.IsConsideredMechanicalAndroid = AccessTools.DeclaredMethod(Type.GetType("ATReforged.Utils,Android Tiers Reforged"), "IsConsideredMechanicalAndroid", [typeof(Pawn)]);
+                    HelperClasses.IsConsideredMechanicalDrone = AccessTools.DeclaredMethod(Type.GetType("ATReforged.Utils,Android Tiers Reforged"), "IsConsideredMechanicalDrone", [typeof(Pawn)]);
+                    HelperClasses.IsConsideredMechanical = AccessTools.DeclaredMethod(Type.GetType("ATReforged.Utils,Android Tiers Reforged"), "IsConsideredMechanical", [typeof(ThingDef)]);
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching Android Tiers Reforged: {ex}");
+                }
             }
             else if (ModsConfig.IsActive("Killathon.MechHumanlikes.AndroidTiersCore"))
             {
-                Settings.ATRActive = true;
-                HelperClasses.IsConsideredMechanicalAndroid = AccessTools.DeclaredMethod(Type.GetType("MechHumanlikes.MHC_Utils,Mechanical Humanlikes Core"), "IsConsideredMechanicalAndroid", [typeof(Pawn)]);
-                HelperClasses.IsConsideredMechanicalDrone = AccessTools.DeclaredMethod(Type.GetType("MechHumanlikes.MHC_Utils,Mechanical Humanlikes Core"), "IsConsideredMechanicalDrone", [typeof(Pawn)]);
-                HelperClasses.IsConsideredMechanical = AccessTools.DeclaredMethod(Type.GetType("MechHumanlikes.MHC_Utils,Mechanical Humanlikes Core"), "IsConsideredMechanical", [typeof(ThingDef)]);
+                try
+                {
+                    Settings.ATRActive = true;
+                    HelperClasses.IsConsideredMechanicalAndroid = AccessTools.DeclaredMethod(Type.GetType("MechHumanlikes.MHC_Utils,Mechanical Humanlikes Core"), "IsConsideredMechanicalAndroid", [typeof(Pawn)]);
+                    HelperClasses.IsConsideredMechanicalDrone = AccessTools.DeclaredMethod(Type.GetType("MechHumanlikes.MHC_Utils,Mechanical Humanlikes Core"), "IsConsideredMechanicalDrone", [typeof(Pawn)]);
+                    HelperClasses.IsConsideredMechanical = AccessTools.DeclaredMethod(Type.GetType("MechHumanlikes.MHC_Utils,Mechanical Humanlikes Core"), "IsConsideredMechanical", [typeof(ThingDef)]);
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching MH: Android Tiers Core: {ex}");
+                }
             }
             if (ModsConfig.IsActive("Neronix17.TweaksGalore"))
             {
-                MethodInfo IsSexualityTrait = AccessTools.DeclaredMethod(Type.GetType("TweaksGalore.Patch_PawnGenerator_GenerateTraits,TweaksGalore"), "IsSexualityTrait");
-                harmony.Patch(IsSexualityTrait, prefix: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.IsSexualityTraitPrefix)));
+                try
+                {
+                    MethodInfo IsSexualityTrait = AccessTools.DeclaredMethod(Type.GetType("TweaksGalore.Patch_PawnGenerator_GenerateTraits,TweaksGalore"), "IsSexualityTrait");
+                    harmony.Patch(IsSexualityTrait, prefix: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.IsSexualityTraitPrefix)));
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching Tweaks Galore: {ex}");
+                }
             }
             if (ModsConfig.IsActive("vanillaracesexpanded.highmate"))
             {
-                Settings.VREHighmateActive = true;
-                harmony.PatchVRE();
+                try
+                {
+                    Settings.VREHighmateActive = true;
+                    harmony.PatchVRE();
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching Vanilla Races Expanded - Highmate: {ex}");
+                }
             }
             if (ModsConfig.IsActive("VanillaExpanded.VanillaSocialInteractionsExpanded"))
             {
-                MethodInfo GetSpouseOrLoverOrFiance = Type.GetType("VanillaSocialInteractionsExpanded.VSIE_Utils, VanillaSocialInteractionsExpanded").GetMethod("GetSpouseOrLoverOrFiance");
-                MethodInfo prefixDelegate = null;
-                foreach (Type t in Type.GetType("VanillaSocialInteractionsExpanded.AddDirectRelation_Patch, VanillaSocialInteractionsExpanded").GetNestedTypes(AccessTools.all).Where((Type t) => t.GetCustomAttribute<System.Runtime.CompilerServices.CompilerGeneratedAttribute>() != null))
+                try
                 {
-                    foreach (MethodInfo method in t.GetMethods(AccessTools.all))
+                    MethodInfo GetSpouseOrLoverOrFiance = Type.GetType("VanillaSocialInteractionsExpanded.VSIE_Utils, VanillaSocialInteractionsExpanded").GetMethod("GetSpouseOrLoverOrFiance");
+                    MethodInfo prefixDelegate = null;
+                    foreach (Type t in Type.GetType("VanillaSocialInteractionsExpanded.AddDirectRelation_Patch, VanillaSocialInteractionsExpanded").GetNestedTypes(AccessTools.all).Where((Type t) => t.GetCustomAttribute<System.Runtime.CompilerServices.CompilerGeneratedAttribute>() != null))
                     {
-                        if (method.ReturnType == typeof(void) && method.Name != "Finalize")
+                        foreach (MethodInfo method in t.GetMethods(AccessTools.all))
                         {
-                            if (prefixDelegate != null)
+                            if (method.ReturnType == typeof(void) && method.Name != "Finalize")
                             {
-                                LogUtil.Error($"Multiple matching methods found: {prefixDelegate.Name} and {method.Name}");
+                                if (prefixDelegate != null)
+                                {
+                                    LogUtil.Error($"Multiple matching methods found: {prefixDelegate.Name} and {method.Name}");
+                                }
+                                prefixDelegate = method;
+                                VSIEPatches.CompilerType = t;
                             }
-                            prefixDelegate = method;
-                            VSIEPatches.CompilerType = t;
                         }
                     }
+                    harmony.Patch(GetSpouseOrLoverOrFiance, postfix: new(typeof(VSIEPatches), nameof(VSIEPatches.GetSpouseOrLoverOrFiancePostfix)));
+                    harmony.Patch(prefixDelegate, transpiler: new(typeof(VSIEPatches), nameof(VSIEPatches.AddDirectRelation_PrefixTranspiler)));
                 }
-                harmony.Patch(GetSpouseOrLoverOrFiance, postfix: new(typeof(VSIEPatches), nameof(VSIEPatches.GetSpouseOrLoverOrFiancePostfix)));
-                harmony.Patch(prefixDelegate, transpiler: new(typeof(VSIEPatches), nameof(VSIEPatches.AddDirectRelation_PrefixTranspiler)));
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching Vanilla Social Interactions Expanded: {ex}");
+                }
             }
             if (ModsConfig.IsActive("rim.job.world") || ModsConfig.IsActive("safe.job.world"))
             {
-                harmony.Patch(AccessTools.DeclaredMethod(Type.GetType("rjw.xxx,RJW"), "is_asexual"), postfix: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.RJWAsexualPostfix)));
-                harmony.Patch(AccessTools.DeclaredMethod(Type.GetType("rjw.CompRJW,RJW"), "VanillaTraitCheck"), transpiler: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.VanillaTraitCheckTranspiler)));
+                try
+                {
+                    harmony.Patch(AccessTools.DeclaredMethod(Type.GetType("rjw.xxx,RJW"), "is_asexual"), postfix: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.RJWAsexualPostfix)));
+                    harmony.Patch(AccessTools.DeclaredMethod(Type.GetType("rjw.CompRJW,RJW"), "VanillaTraitCheck"), transpiler: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.VanillaTraitCheckTranspiler)));
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching RimJobWorld: {ex}");
+                }
             }
             if (ModsConfig.IsActive("Neronix17.Asimov"))
             {
-                Settings.AsimovActive = true;
-                HelperClasses.IsHumanlikeAutomaton = AccessTools.DeclaredMethod(Type.GetType("Asimov.AutomatonUtil,Asimov"), "IsHumanlikeAutomaton");
-                HelperClasses.PawnDef = Type.GetType("Asimov.PawnDef,Asimov");
-                HelperClasses.pawnSettings = AccessTools.Field(HelperClasses.PawnDef, "pawnSettings");
-                HelperClasses.AsimovGrowth = AccessTools.Field(Type.GetType("Asimov.PawnSettings,Asimov"), "hasGrowthMoments");
+                try
+                {
+                    Settings.AsimovActive = true;
+                    HelperClasses.IsHumanlikeAutomaton = AccessTools.DeclaredMethod(Type.GetType("Asimov.AutomatonUtil,Asimov"), "IsHumanlikeAutomaton");
+                    HelperClasses.PawnDef = Type.GetType("Asimov.PawnDef,Asimov");
+                    HelperClasses.pawnSettings = AccessTools.Field(HelperClasses.PawnDef, "pawnSettings");
+                    HelperClasses.AsimovGrowth = AccessTools.Field(Type.GetType("Asimov.PawnSettings,Asimov"), "hasGrowthMoments");
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching Asimov: {ex}");
+                }
             }
             if (ModsConfig.IsActive("RforReload.EdgesOfAcceptence"))
             {
-                harmony.PatchForceLoveHate();
+                try
+                {
+                    harmony.PatchForceLoveHate();
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching Edges of Acceptance: {ex}");
+                }
             }
             if (ModsConfig.IsActive("Dra.RimderRomanceControl"))
             {
-                harmony.Patch(AccessTools.DeclaredMethod(Type.GetType("Rimder.HarmonyPatches.ChangeStatusOnRelationshipChange,RimderRomanceControl"), "Postfix"), transpiler: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.RimderLoveRelationTranspiler)));
-                harmony.Patch(AccessTools.DeclaredMethod(Type.GetType("Rimder.HarmonyPatches.ChangeStatusOnRelationshipChange,RimderRomanceControl"), "Postfix"), transpiler: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.RimderExLoveRelationTranspiler)));
+                try
+                {
+                    harmony.Patch(AccessTools.DeclaredMethod(Type.GetType("Rimder.HarmonyPatches.ChangeStatusOnRelationshipChange,RimderRomanceControl"), "Postfix"), transpiler: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.RimderLoveRelationTranspiler)));
+                    harmony.Patch(AccessTools.DeclaredMethod(Type.GetType("Rimder.HarmonyPatches.ChangeStatusOnRelationshipChange,RimderRomanceControl"), "Postfix"), transpiler: new(typeof(OtherMod_Patches), nameof(OtherMod_Patches.RimderExLoveRelationTranspiler)));
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching Rimder: Romance Control: {ex}");
+                }
             }
             if (ModsConfig.IsActive("tachyonite.pawnmorpherpublic"))
             {
-                Settings.PawnmorpherActive = true;
-                harmony.PatchPawnmorpher();
-                HelperClasses.IsHumanlikePM = AccessTools.Method(Type.GetType("Pawnmorph.FormerHumanUtilities, Pawnmorph"), "IsHumanlike");
+                try
+                {
+                    Settings.PawnmorpherActive = true;
+                    harmony.PatchPawnmorpher();
+                    HelperClasses.IsHumanlikePM = AccessTools.Method(Type.GetType("Pawnmorph.FormerHumanUtilities, Pawnmorph"), "IsHumanlike");
+                }
+                catch (Exception ex)
+                {
+                    LogUtil.Error($"Error encountered while patching Pawnmorpher: {ex}");
+                }
             }
 
             MakeFertilityModList();
