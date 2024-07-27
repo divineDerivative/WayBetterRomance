741c68895a990b4b95041c580f101aed0e77fc2c
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-07-27 16:06:30 -0500

    Make comp save and load properly

--- Source/Orientation Comp/Comp_Orientation.cs
+++ Source/Orientation Comp/Comp_Orientation.cs
@@ -141,7 +141,9 @@ namespace BetterRomance
         {
             base.Initialize(props);
             sexual = new AttractionVars();
+            sexual.gender = Pawn.gender;
             romantic = new AttractionVars();
+            romantic.gender = Pawn.gender;
         }
 
         public override void PostExposeData()
