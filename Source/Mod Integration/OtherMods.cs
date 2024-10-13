b051abf847d06c659cb3d0d09a6adc99663e4d37
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-10-13 13:03:56 -0500

    Use DivineFramework for logging

--- Source/Mod Integration/OtherMods.cs
+++ Source/Mod Integration/OtherMods.cs
@@ -2,7 +2,6 @@ using HarmonyLib;
 using RimWorld;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using VanillaRacesExpandedHighmate;
