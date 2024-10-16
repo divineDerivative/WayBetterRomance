6459c2ccfc44bc10642e15590fee989fdbe0944e
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-10-16 17:08:08 -0500

    Use DivineFramework for settings page

--- Source/OnStartup.cs
+++ Source/OnStartup.cs
@@ -198,6 +198,7 @@ namespace BetterRomance
             }
 
             MakeFertilityModList();
+            Settings.JoyNeed = DefDatabase<NeedDef>.GetNamed("Joy");
             Settings.ApplyJoySettings();
             MakeRaceSettingsList();
         }
