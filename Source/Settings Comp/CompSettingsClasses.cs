5f02304a25ef1b1dff49e6e3b4ecdcf886ed9a0e
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-11-26 12:30:21 -0600

    New class for gender specific attraction chances

--- Source/Settings Comp/CompSettingsClasses.cs
+++ Source/Settings Comp/CompSettingsClasses.cs
@@ -78,6 +78,31 @@ namespace BetterRomance
         }
     }
 
+    public class GenderAttractionChances
+    {
+        public float men = 0.5f;
+        public float women = 0.5f;
+        public float enby = 0.25f;
+
+        public float notMen => 1f - men;
+        public float notWomen => 1f - women;
+        public float notEnby => 1f- enby;
+
+        public void CopyFrom(GenderAttractionChances other)
+        {
+            men = other.men;
+            women = other.women;
+            enby = other.enby;
+        }
+
+        public void Reset()
+        {
+            men = 0.5f;
+            women = 0.5f;
+            enby = 0.25f;
+        }
+    }
+
     public class CompSettingsCasualSexRace
     {
         public bool? caresAboutCheating;
