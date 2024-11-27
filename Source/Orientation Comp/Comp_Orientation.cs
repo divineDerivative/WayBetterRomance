cadddf2231bf7cadc33b5ef0a0ece20e8cf279b1
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-11-27 12:55:34 -0600

    Grammar fix

--- Source/Orientation Comp/Comp_Orientation.cs
+++ Source/Orientation Comp/Comp_Orientation.cs
@@ -52,7 +52,6 @@ namespace BetterRomance
 
             internal string GenderString()
             {
-                //Test this
                 return Genders switch
                 {
                     AttractionGenders.None => null,
