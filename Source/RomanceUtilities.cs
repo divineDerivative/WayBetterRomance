5d80e39898042f6167d009892b5058c90f77dd26
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-12-22 09:03:35 -0600

    Look at gender of existing partners

--- Source/RomanceUtilities.cs
+++ Source/RomanceUtilities.cs
@@ -3,6 +3,7 @@ using HarmonyLib;
 using RimWorld;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
@@ -453,6 +454,11 @@ namespace BetterRomance
                 return true;
             }
             return false;
+        }
+
+        public static bool HasAnyLovePartnerOfGender(this Pawn pawn, Gender gender)
+        {
+            return pawn.relations.DirectRelations.Any(x => LovePartnerRelationUtility.IsLovePartnerRelation(x.def) && x.otherPawn.gender == gender);
         }
     }
 
