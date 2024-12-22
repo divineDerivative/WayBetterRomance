dce9bee3a57e8115e62256ce69dc1e8ad8c9c6cc
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2024-12-22 09:03:35 -0600

    Look at gender of existing partners

--- Source/RomanceUtilities.cs
+++ Source/RomanceUtilities.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using HarmonyLib;
@@ -447,6 +448,11 @@ namespace BetterRomance
                 return true;
             }
             return false;
+        }
+
+        public static bool HasAnyLovePartnerOfGender(this Pawn pawn, Gender gender)
+        {
+            return pawn.relations.DirectRelations.Any(x => LovePartnerRelationUtility.IsLovePartnerRelation(x.def) && x.otherPawn.gender == gender);
         }
     }
 
