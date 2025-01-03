7f46d01201667146109fe723f804c14751c6fbe1
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2025-01-03 11:22:07 -0600

    DivineFramework update

--- Source/Settings Page/BetterRomanceMod.cs
+++ Source/Settings Page/BetterRomanceMod.cs
@@ -14,7 +14,7 @@ namespace BetterRomance
         public BetterRomanceMod(ModContentPack content) : base(content)
         {
             settings = GetSettings<Settings>();
-            ModManagement.RegisterMod("WBR.WayBetterRomance", typeof(BetterRomanceMod).Assembly.GetName().Name, new("0.5.0.0"), "<color=#1116e4>[WayBetterRomance]</color>", () => Settings.debugLogging);
+            ModManagement.RegisterMod("WBR.WayBetterRomance", typeof(BetterRomanceMod).Assembly.GetName().Name, new("0.8.0.0"), "<color=#1116e4>[WayBetterRomance]</color>", () => Settings.debugLogging);
         }
 
         public override string SettingsCategory()
