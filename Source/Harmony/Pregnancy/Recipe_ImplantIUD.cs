afb42f138424d130cea29fa01685884b2f527851
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2025-07-29 12:56:52 -0500

    Compatibility with Persona Mechanoid Pawns 2

--- Source/Harmony/Pregnancy/Recipe_ImplantIUD.cs
+++ Source/Harmony/Pregnancy/Recipe_ImplantIUD.cs
@@ -2,6 +2,7 @@
 using RimWorld;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using Verse;
 
 namespace BetterRomance.HarmonyPatches
 {
@@ -9,5 +10,15 @@ namespace BetterRomance.HarmonyPatches
     public static class Recipe_ImplantIUD_AvailableOnNow
     {
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) => instructions.AgeToHaveChildrenInt(OpCodes.Ldloc_0);
+
+        public static bool Prefix(Thing thing, ref bool __result)
+        {
+            if (thing is Pawn pawn && pawn.gender is Gender.Female or Gender.Male)
+            {
+                return true;
+            }
+            __result = false;
+            return false;
+        }
     }
 }
